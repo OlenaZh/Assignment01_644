8eb719d5b273ee253f9c15f8915098237e982c6d
OlenaZh <alyona.mesh@gmail.com>
2022-02-23 19:05:51 -0800

    done

---
+++ js/script.js
@@ -0,0 +1,143 @@
+// STEP 1 Convert the following highlighted identifiers to Camel Case notation
+    //let someMonth;
+    //function theMonth();
+    //let currentMonth;
+    //let summerMonth;
+    //let myLibraryFunction;
+
+// STEP 2 Examples of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+    //20                //numeric literal expression
+    //"Hello World!"   // a string literal expression
+    //true            // a Boolean literal expression
+    //null           // a null literal expression
+
+// STEP 3 Two examples of complex / variable expressions.
+    //let anExpression = 3 *(5/8) + 7;
+    //let sum = x + y; 
+    //let total = sum * 100 / .5; 
+
+
+// STEP 4 Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+    //let strFirstName;
+    //let strLastName;
+    //let strAddress;
+    //let strCity;
+    //let strState;
+    //let strZipCode;
+    //let numYourAge;
+    //let strReferralSource;
+    //let blnMayWeContactYou;
+
+// STEP 5 Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables
+
+    // WAY 1
+    //let firstName;
+    //let lastName;
+    //let mayWeContactYou;
+    //firstName = "Olena";
+    //lastName = "Zheliabina";
+    //mayWeContactYou = true;
+
+    // WAY 2
+    //let firstName = "Olena";
+    //let lastName = "Zheliabina";
+    //let mayWeContactYou = true;
+
+    //WAY 3
+    //let firstName = "Olena", lastName = "Zheliabina", mayWeContactYou = rtue;
+
+// STEP 6 Create a variable. Add a number and a string and display the coerced result in the browser’s console window.
+    //let favoriteNumber = "My favorite Number is " + 7; 
+    //window.console.log (favoriteNumber);
+
+// STEP 7 Create two variables. For the first variable, add a Boolean and a string and display the coerced result. For the second variable, add a number and a Boolean and display the coerced result. 
+    //let mayWeContactYou = "May we contact you? " + true;
+    //window.console.log (mayWeContactYou);
+
+    //let myFavoriteNumber = 7 + true;
+    //window.console.log (myFavoriteNumber);
+
+// STEP 8
+    //var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+    //window.console.log(someString);
+
+// STEP 9
+    //let myVariable = null;
+    //let myVariable2;
+    //window.console.log(myVariable);
+    //window.console.log(myVariable2);
+
+// STEP 10
+    //let firstName = "Olena";
+    //let numPhone = 555-55-55;
+    //let isStudent = true;
+    //let car = new Object();
+    //let myVariable;
+    //window.console.log(typeof firstName);
+    //window.console.log(typeof numPhone);
+    //window.console.log(typeof isStudent);
+    //window.console.log(typeof car);
+    //window.console.log(typeof myVariable);
+
+// STEP 11 Within an alert box, use the concatenation operator (+) to display text in the alert box that appears as follows: Hello Zak Ruvalcaba, welcome to the JavaScript class!
+    //window.alert("Hello " + "Olena Zheliabina" + ", welcome to the JavaScript class!");
+
+// STEP 12	Declare a variable called name and set it equal to your name. Substitute your name in the previous alert string with the variable instead.
+    //let name = "Olena Zheliabina";
+    //window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 13 Declare a variable called course and set it equal to “JavaScript”. Rework your alert string so that it displays the string of text but using the variables as opposed to hard coded text.
+    //let name = "Olena Zheliabina";
+    //let course = "JavaScript";
+    //window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 14 Rework the above string so that a line break is added right before the word “Welcome”. Your alert box should display as follows.
+    //let name = "Olena Zheliabina";
+    //let course = "JavaScript";
+    //window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+
+// STEP 15 Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable.
+    //let name = window.prompt("What is your name?");
+    //let course = "JavaScript";
+    //window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 16 Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
+    //let name = window.prompt("What is your name?");
+    //let course = window.prompt("What class are you taking?");
+    //window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 17 Declare a variable called x and assign it a value of 10. Declare a variable called y and assign it a value of 20. Display the sum of those two numbers in the console window.
+    //let x = 10;
+    //let y = 20;
+    //window.console.log(x + y); 
+
+// STEP 18 Declare a variable called x and assign it a value of 20. Add and assign 20 to that variable and display the result in the console window.The result should be 40.
+    //let x = 20;
+    //window.console.log(x+=20);
+
+
+// STEP 19 Declare a variable called x and assign it a value of 20. Multiply and assign 5 to that variable and display the result in the console window. The result should be 100.
+    //let x = 20;
+    //window.console.log(x*=5);
+
+// STEP 20 Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3.Divide and assign 1 to that variable and display the result in the console window. The result should be 2. If you got 6.66 try again.
+    //let x = 20 % 3; 
+    //window.console.log(x/=1);
+
+// STEP 21 Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result within the console window.
+    //let x = 20;
+    //let y = 10;
+    //if (x > 10 || y < 5) {
+    //window.console.log("Congrats!");
+    //}
+
+// STEP 22	Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result within the console window. The application cannot use the same operators used in the previous application.
+    //let x = 20;
+    //let y = 10;
+    //if (x === 20 && y !== 10) {
+    //    window.console.log("true");
+    //} else {
+    // window.console.log("false");
+    //}
+
